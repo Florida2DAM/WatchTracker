607a155ee06653979118ad9239ff5664447fdc5e
Jose Lacueva <joslacmen@gmail.com>
2021-02-01 19:05:41 +0100

    Instalación de librerías React Native Elements y Vector Icons. Creación pantallas en blanco para Login y Registro.

---
+++ FrontendWT/screens/Login.js
@@ -0,0 +1,25 @@
+import { Button, Icon } from 'react-native-elements';
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+
+
+export default class Login extends React.Component {
+
+    render() {
+
+        return (
+            <View>
+                <Text>Login Screen</Text>
+            </View>
+        );
+
+    }
+
+}
+
+const structureStyles = StyleSheet.create({
+    mainStyle: {
+
+    }
+});
---
+++ FrontendWT/screens/Register.js
@@ -0,0 +1,25 @@
+import { Button, Icon } from 'react-native-elements';
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+
+
+export default class Register extends React.Component {
+
+    render() {
+        
+        return (
+            <View>
+                <Text>Register Screen</Text>
+            </View>
+        );
+
+    }
+
+}
+
+const structureStyles = StyleSheet.create({
+    mainStyle: {
+
+    }
+});
