706dc949cd6f21e4ca6073b4f4cf3e99d96688ed
jolame <jolame@floridauniversitaria.es>
2021-02-10 17:27:28 +0100

    Backoffice Versión 0.1.0:
    - Añadida vista genérica con sus estilos.
    
    - Añadidas librerías React-Router-Dom y PrimeReact.
    
    - Cambios a los iconos y estructura general de la aplicación.

---
+++ backoffice_wt/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import {Switch, Route, Redirect, NavLink, BrowserRouter} from "react-router-dom";
+
+function App() {
+  return (
+      <div className="App">
+          <BrowserRouter>
+              <div className={'menu'}>
+                  <h2>Watch Tracker Backoffice</h2>
+                  <ul>
+                      <li><NavLink to={'/users'} className={'menu-default'} activeClassName={'menu-active'}>Users</NavLink></li>
+                      <li><NavLink to={'/events'} className={'menu-default'} activeClassName={'menu-active'}>Providers</NavLink></li>
+                      <li><NavLink to={'/reports'} className={'menu-default'} activeClassName={'menu-active'}>Reports</NavLink></li>
+                  </ul>
+              </div>
+              <div className={'display'}>
+                  <Switch>
+                      <Route path={'/users'}></Route>
+                      <Route path={'/events'}></Route>
+                      <Route path={'/reports'}></Route>
+                      <Redirect exact from="/" to="users"/>
+                  </Switch>
+              </div>
+          </BrowserRouter>
+      </div>
+  );
+}
+
+export default App;
